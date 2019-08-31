40a86c5b2e6852652c362acfd96a7219887ae6b4
Melanie <melanie_reyna@usmp.pee>
2019-08-31 10:56:14 -0500

    Primeros cambios

--- Codigos/gitCodigo.cs
+++ Codigos/gitCodigo.cs
@@ -1,4 +1,21 @@
-using (resource);
+using System;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_web.models;
+
+namespace dotnet_web.Controllers
 {
-    
-}
+
+    public class Homecontroller:Controller{
+
+
+        public IActionResult Index(){
+            Console.WriteLine("Index");
+            return View();
+        }
+        
+   
+        }
+    }
+
+
+
